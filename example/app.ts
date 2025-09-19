49c4cd9f1041a701fc4abfa72c0c436e678392fd
aldia_mechdream <futami16237@gmail.com>
2025-09-19 19:09:16 +0800

    chore: 添加@emotion/css用于测试； 删除旧的example，vite新建一个； 试作发布0.0.4

--- example/app.ts
+++
@@ -1,6 +0,0 @@
-import { useScope } from '@/index.js';
-
-const { h } = useScope();
-
-// todo 配置一个能跑起来的测试看看
-export default h('div', 'container').append(h('h2', 'title').append(h('span', undefined, 'Test')));
--- example/main.ts
+++
@@ -1,8 +0,0 @@
-// Code Language: TypeScript
-// 引入样式
-import './css/style.css';
-
-// 引入模块
-import app from './app.js';
-
-app.mount(document.body);

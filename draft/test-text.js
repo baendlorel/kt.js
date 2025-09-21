ff7db57b76f412847555bbd141b7ee16dded97b0
aldia_mechdream <futami16237@gmail.com>
2025-09-21 13:05:12 +0800

    test: 测试用的文件最终是一个非常老的写法，去掉了?. ，去掉了arrayfrom，去掉了queryselector，去掉了includes； 因为没有prepend，所以加在全部内容前面改用header布局

--- draft/test-text.js
+++ draft/test-text.js
@@ -1,195 +1,272 @@
-{
-  const TEXT_CONTENT = '文本节点';
-  const BLOCK = 'block';
-  const TAG = 'tag';
+// @ts-check
 
-  function testTag(tag) {
-    const wrapper = document.createElement('div');
-    wrapper.className = BLOCK;
-    wrapper.style.display = 'flex';
-    wrapper.style.marginBottom = '8px';
+var ArraySlice = Array.prototype.slice;
+var TEXT_CONTENT = '文本节点2';
+var WRAPPER = 'block';
+var EL = 'EL';
 
-    const el = document.createElement(tag);
-    el.className = TAG;
+var header = document.createElement('div');
+var container = document.createElement('div');
 
-    const span = document.createElement('span');
-    span.textContent = tag + ' : ';
-    span.style.fontWeight = 'bold';
-
-    const textNode = document.createTextNode(TEXT_CONTENT);
-    el.appendChild(textNode);
-    wrapper.append(span, el);
-    document.body.appendChild(wrapper);
+function getBrowserName() {
+  var ua = navigator && navigator.userAgent;
+  if (!ua) {
+    return 'Unknown';
   }
 
-  // find which tags cannot render text nodes
-  function collect() {
-    const invalidTextNodeTags = [];
-    const list = document.querySelectorAll('.' + BLOCK);
-    for (let i = 0; i < list.length; i++) {
-      const wrapper = list[i];
-      if (!wrapper.textContent.includes(TEXT_CONTENT)) {
-        invalidTextNodeTags.push(wrapper.querySelector('.' + TAG).tagName);
+  if (/Edg\//i.test(ua)) return 'Edge';
+  if (/Chrome\//i.test(ua)) {
+    if (/QQBrowser/i.test(ua)) return 'QQ Browser';
+    if (/UBrowser/i.test(ua)) return 'UC Browser';
+    if (/MetaSr/i.test(ua)) return 'Sogou Browser';
+    if (/2345Explorer/i.test(ua)) return '2345 Explorer';
+    if (/LBBROWSER/i.test(ua)) return 'Liebao Browser';
+    if (/Maxthon/i.test(ua)) return 'Maxthon';
+    if (/OPR\//i.test(ua)) return 'Opera';
+    return 'Chrome';
+  }
+  if (/Firefox\//i.test(ua)) return 'Firefox';
+  if (/Safari\//i.test(ua)) return 'Safari';
+  return JSON.stringify(ua);
+}
+
+/**
+ *
+ * @param {string} tag
+ */
+function testTag(tag) {
+  var wrapper = document.createElement('div');
+  wrapper.style.display = 'flex';
+  wrapper.style.marginBottom = '8px';
+  wrapper.classList.add(WRAPPER);
+
+  var el = document.createElement(tag);
+  el.classList.add(EL);
+
+  var span = document.createElement('span');
+  span.textContent = tag + ' : ';
+  span.style.fontWeight = 'bold';
+
+  // ! Adding a text node to <script> causes Maxthon to throw an uncatchable error.
+  // * just skip <script>, we already know it is invalid
+  if (tag === 'script') {
+    return;
+  }
+
+  var textNode = document.createTextNode(TEXT_CONTENT);
+  el.appendChild(textNode);
+
+  wrapper.appendChild(span);
+  wrapper.appendChild(el);
+  container.appendChild(wrapper);
+}
+
+// find which tags cannot render text nodes
+function collect() {
+  var invalidTextNodeTags = [];
+  /**
+   * @type {HTMLElement[]}
+   */
+  var list = ArraySlice.call(document.getElementsByClassName(WRAPPER));
+
+  for (var i = 0; i < list.length; i++) {
+    /**
+     * @type {HTMLElement}
+     */
+    var wrapper = list[i];
+    if (wrapper.innerText.indexOf(TEXT_CONTENT) === -1) {
+      var w = wrapper.getElementsByClassName(EL)[0];
+      if (!w) {
+        alert('没有找到' + EL);
+        return;
+      }
+      if (invalidTextNodeTags.length % 10 === 0) {
+        invalidTextNodeTags.push('\n' + w.tagName);
+      } else {
+        invalidTextNodeTags.push(w.tagName);
       }
     }
-
-    const pre = document.createElement('pre');
-    pre.style.fontFamily = 'JetBrains Mono';
-    pre.style.width = '42rem';
-    pre.style.fontSize = '16px';
-    pre.style.margin = '20px';
-    pre.style.color = '#c12525';
-    pre.style.textWrap = 'auto';
-    pre.textContent = invalidTextNodeTags.join(', ').toLowerCase();
-    document.body.appendChild(pre);
   }
 
-  function run() {
-    const body = document.body;
-    body.innerHTML = '';
+  var pre = document.createElement('pre');
+  pre.style.fontFamily = 'JetBrains Mono';
+  pre.style.width = '42rem';
+  pre.style.fontSize = '16px';
+  pre.style.margin = '20px';
+  pre.style.color = '#c12525';
+  pre.textContent = getBrowserName() + ' :\n' + invalidTextNodeTags.join(', ').toLowerCase();
 
-    const htmlTags = [
-      'a',
-      'abbr',
-      'acronym',
-      'address',
-      'applet',
-      'area',
-      'article',
-      'aside',
-      'audio',
-      'b',
-      'base',
-      'basefont',
-      'bdi',
-      'bdo',
-      'big',
-      'blockquote',
-      'body',
-      'br',
-      'button',
-      'canvas',
-      'caption',
-      'center',
-      'cite',
-      'code',
-      'col',
-      'colgroup',
-      'data',
-      'datalist',
-      'dd',
-      'del',
-      'details',
-      'dfn',
-      'dialog',
-      'dir',
-      'div',
-      'dl',
-      'dt',
-      'em',
-      'embed',
-      'fieldset',
-      'figcaption',
-      'figure',
-      'font',
-      'footer',
-      'form',
-      'frame',
-      'frameset',
-      'h1',
-      'h2',
-      'h3',
-      'h4',
-      'h5',
-      'h6',
-      'head',
-      'header',
-      'hgroup',
-      'hr',
-      'html',
-      'i',
-      'iframe',
-      'img',
-      'input',
-      'ins',
-      'kbd',
-      'keygen',
-      'label',
-      'legend',
-      'li',
-      'link',
-      'main',
-      'map',
-      'mark',
-      'marquee',
-      'menu',
-      'menuitem',
-      'meta',
-      'meter',
-      'nav',
-      'nobr',
-      'noembed',
-      'noframes',
-      'noscript',
-      'object',
-      'ol',
-      'optgroup',
-      'option',
-      'output',
-      'p',
-      'param',
-      'picture',
-      'plaintext',
-      'portal',
-      'pre',
-      'progress',
-      'q',
-      'rb',
-      'rp',
-      'rt',
-      'rtc',
-      'ruby',
-      's',
-      'samp',
-      'script',
-      'search',
-      'section',
-      'select',
-      'slot',
-      'small',
-      'source',
-      'spacer',
-      'span',
-      'strike',
-      'strong',
-      'style',
-      'sub',
-      'summary',
-      'sup',
-      'table',
-      'tbody',
-      'td',
-      'template',
-      'textarea',
-      'tfoot',
-      'th',
-      'thead',
-      'time',
-      'title',
-      'tr',
-      'track',
-      'tt',
-      'u',
-      'ul',
-      'var',
-      'video',
-      'wbr',
-      'xmp',
-    ];
+  header.appendChild(pre);
+}
 
-    htmlTags.forEach(testTag);
-    setTimeout(collect, 1000);
-  }
+function run() {
+  document.body.innerHTML = '';
+  document.body.appendChild(header);
+  document.body.appendChild(container);
 
+  var htmlTags = [
+    'a',
+    'abbr',
+    'acronym',
+    'address',
+    'appvar',
+    'area',
+    'article',
+    'aside',
+    'audio',
+    'b',
+    'base',
+    'basefont',
+    'bdi',
+    'bdo',
+    'big',
+    'blockquote',
+    'body',
+    'br',
+    'button',
+    'canvas',
+    'caption',
+    'center',
+    'cite',
+    'code',
+    'col',
+    'colgroup',
+    'data',
+    'datalist',
+    'dd',
+    'del',
+    'details',
+    'dfn',
+    'dialog',
+    'dir',
+    'div',
+    'dl',
+    'dt',
+    'em',
+    'embed',
+    'fieldset',
+    'figcaption',
+    'figure',
+    'font',
+    'footer',
+    'form',
+    'frame',
+    'frameset',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'head',
+    'header',
+    'hgroup',
+    'hr',
+    'html',
+    'i',
+    'iframe',
+    'img',
+    'input',
+    'ins',
+    'kbd',
+    'keygen',
+    'label',
+    'legend',
+    'li',
+    'link',
+    'main',
+    'map',
+    'mark',
+    'marquee',
+    'menu',
+    'menuitem',
+    'meta',
+    'meter',
+    'nav',
+    'nobr',
+    'noembed',
+    'noframes',
+    'noscript',
+    'object',
+    'ol',
+    'optgroup',
+    'option',
+    'output',
+    'p',
+    'param',
+    'picture',
+    'plaintext',
+    'portal',
+    'pre',
+    'progress',
+    'q',
+    'rb',
+    'rp',
+    'rt',
+    'rtc',
+    'ruby',
+    's',
+    'samp',
+    'script',
+    'search',
+    'section',
+    'select',
+    'slot',
+    'small',
+    'source',
+    'spacer',
+    'span',
+    'strike',
+    'strong',
+    'style',
+    'sub',
+    'summary',
+    'sup',
+    'table',
+    'tbody',
+    'td',
+    'template',
+    'textarea',
+    'tfoot',
+    'th',
+    'thead',
+    'time',
+    'title',
+    'tr',
+    'track',
+    'tt',
+    'u',
+    'ul',
+    'var',
+    'video',
+    'wbr',
+    'xmp',
+  ];
+
+  htmlTags.forEach(testTag);
+  setTimeout(() => {
+    try {
+      collect();
+    } catch (/**  @type {any} */ error) {
+      if (typeof error === 'string') {
+        alert(error);
+      } else if (typeof error === 'object' && error !== null) {
+        alert(error.message);
+      } else {
+        alert(String(error));
+      }
+    }
+  }, 1000);
+}
+
+try {
   run();
+} catch (/**  @type {any} */ error) {
+  if (typeof error === 'string') {
+    alert(error);
+  } else if (typeof error === 'object' && error !== null) {
+    alert(error.message);
+  } else {
+    alert(String(error));
+  }
 }

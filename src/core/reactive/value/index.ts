9d4f1c843af2102a31cfc8a10168047d6223004c
aldia_mechdream <futami16237@gmail.com>
2025-09-22 21:28:50 +0800

    feat: 考虑到临时对象会被引擎快速标记为可回收，所以不再战战兢兢地避免对象了； 删掉不需要的类型标注；

--- src/core/reactive/value/index.ts
+++ src/core/reactive/value/index.ts
@@ -1,187 +1,67 @@
-import { deferedBranchDynamic } from 'defered-branch';
 import { KValueSimple } from './simple.js';
 import { KValue } from './normal.js';
 import { bindValueAsDate, bindValueAsNumber } from './binders.js';
 import { trivial } from './transformers.js';
 
-type KValueReturn<T, E extends HTMLKEnhancedElement, Args extends any[]> =
-  Args extends KValueArgs0<T, E>
-    ? KValueSimple<T>
-    : Args extends KValueArgs1<T, E>
-      ? E extends HTMLKEnhancedElement<infer TagName>
-        ? TagName extends 'input'
-          ? T extends number
-            ? KValueSimple<number>
-            : T extends Date
-              ? KValueSimple<Date>
-              : KValueSimple<string>
-          : TagName extends 'select'
-            ? KValueSimple<string>
-            : TagName extends 'textarea'
-              ? KValueSimple<string>
-              : KValue<string> // * fallback
-        : KValue<string> // * fallback
-      : Args extends KValueArgs3<T, E> // [E, string, Transform<T>]
-        ? KValue<T>
-        : KValue<string>; // * fallback
-
-type BranchFn0 = <T extends any, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  args: KValueArgs0<T, E>
-) => KValueReturn<T, E, KValueArgs0<T, E>>;
-
-type BranchFn1 = <T extends any, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  args: KValueArgs1<T, E>
-) => KValueReturn<T, E, KValueArgs1<T, E>>;
-
-type BranchFn2 = <T extends any, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  args: KValueArgs2<T, E>
-) => KValueReturn<T, E, KValueArgs2<T, E>>;
-
-type BranchFn3 = <T extends any, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  args: KValueArgs3<T, E>
-) => KValueReturn<T, E, KValueArgs3<T, E>>;
-
-type BranchFn4 = <T extends any, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  args: KValueArgs4<T, E>
-) => KValueReturn<T, E, KValueArgs4<T, E>>;
-
-type BranchFn = BranchFn0 | BranchFn1 | BranchFn2 | BranchFn3 | BranchFn4;
-
-const branch = deferedBranchDynamic<BranchFn>()
-  .add(
-    (_, args) => args.length === 0,
-    (initialValue, args) => {
-      return new KValueSimple<string>(initialValue);
-    }
-  )
-  .add(
-    // & need auto optimization
-    (_, args) => args.length === 1,
-    (initialValue, args) => {
-      const el = args[0] as unknown as HTMLKEnhancedElement;
-      switch (el.tagName) {
-        case 'input': {
-          const elementType = (el as HTMLKEnhancedElement<'input'>).type;
-          if (elementType === 'number') {
-            return bindValueAsNumber(initialValue as number, el as HTMLKEnhancedElement<'input'>);
-          }
-
-          if (elementType === 'checkbox') {
-            return new KValueSimple<boolean>(Boolean(initialValue)).bindChange(el, 'checked');
-          }
-
-          if (elementType === 'radio') {
-            return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
-          }
-
-          if (
-            elementType === 'date' ||
-            elementType === 'time' ||
-            elementType === 'week' ||
-            elementType === 'month'
-          ) {
-            return bindValueAsDate(initialValue as Date, el as HTMLKEnhancedElement<'input'>);
-          }
-
-          // & fallback to value
-          return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
-        }
-        case 'select': {
-          return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
-        }
-        case 'textarea': {
-          return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
-        }
-        default:
-          throw new TypeError(
-            `[__NAME__:kvalue] Cannot automically bind field on <${el.tagName}>, please specify the field manually(you can also provide vtoe and etov transformers).`
-          );
+const autoBind = (initialValue: any, el: HTMLKEnhancedElement) => {
+  switch (el.tagName) {
+    case 'input': {
+      const elementType = (el as HTMLKEnhancedElement<'input'>).type;
+      if (elementType === 'number') {
+        return bindValueAsNumber(initialValue as number, el as HTMLKEnhancedElement<'input'>);
       }
+
+      if (elementType === 'checkbox') {
+        return new KValueSimple<boolean>(Boolean(initialValue)).bindChange(el, 'checked');
+      }
+
+      if (elementType === 'radio') {
+        return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
+      }
+
+      if (
+        elementType === 'date' ||
+        elementType === 'time' ||
+        elementType === 'week' ||
+        elementType === 'month'
+      ) {
+        return bindValueAsDate(initialValue as Date, el as HTMLKEnhancedElement<'input'>);
+      }
+
+      // & fallback to value
+      return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
     }
-  )
-  .add(
-    // & need auto optimization
-    (_, args) => args.length === 2,
-    (initialValue, args) => {
-      return new KValueSimple<string>(String(initialValue)).bindChange(
-        args[0] as unknown as HTMLKEnhancedElement,
-        args[1] as ChangeTriggerField | otherstring
+    case 'select': {
+      return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
+    }
+    case 'textarea': {
+      return new KValueSimple<string>(String(initialValue)).bindChange(el, 'value');
+    }
+    default:
+      throw new TypeError(
+        `[__NAME__:kvalue] Cannot automically bind field on <${el.tagName}>, please specify the field manually(you can also provide vtoe and etov transformers).`
       );
-    }
-  )
-  .add(
-    (_, args) => args.length === 3,
-    (initialValue, args) => {
-      return new KValue<string>(
-        String(initialValue),
-        args[3] as Transform<string>,
-        trivial
-      ).bindChange(args[0] as HTMLKEnhancedElement, args[1] as ChangeTriggerField | otherstring);
-    }
-  )
-  .add(
-    (_, args) => args.length === 4,
-    (initialValue, args) => {
-      return new KValue<string>(
-        String(initialValue),
-        args[3] as Transform<string>,
-        trivial
-      ).bindChange(args[0] as HTMLKEnhancedElement, args[1] as ChangeTriggerField | otherstring);
-    }
-  );
+  }
+};
 
 /**
  * Create a reactive value that can be bound to an element's value or other properties.
  * - the binding is valid **if and only if** when element's field can trigger a change event.
  * @param initialValue the initial value
- */
-export function kvalue<T, E extends HTMLKEnhancedElement>(initialValue: T): KValueSimple<T>;
-
-/**
- * Create a reactive value that can be bound to an element's value or other properties.
- * @param initialValue the initial value
- * @param element the element to bind to
- */
-export function kvalue<T, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  element: E
-): KValueSimple<T>;
-
-/**
- * Create a reactive value that can be bound to an element's value or other properties.
- * - the binding is valid **if and only if** when element's field can trigger a change event.
- * @param initialValue the initial value
- * @param element the element to bind to
- * @param field value field, will be used like `element[field]`
- * @param vtoe transform from value to element's field
- * @param etov transform from element's field to value
  * @returns when vtoe and etov are provided, return KValue<T>, otherwise return KValueSimple<T>
  */
-export function kvalue<T, E extends HTMLKEnhancedElement, EValueType = string>(
-  initialValue: T,
-  element: E,
-  field: ChangeTriggerField | otherstring,
-  vtoe?: Transform<T>,
-  etov?: Transform<EValueType, T>
-): KValue<T>;
-
-export function kvalue<T, E extends HTMLKEnhancedElement>(
-  initialValue: T,
-  ...args: KValueArgs<T, E>
-) {
+export function kvalue<T>(initialValue: T) {
   switch (args.length) {
     case 0:
       return new KValueSimple<T>(initialValue);
     case 1:
+      return autoBind(initialValue, args[0]);
     case 2:
+      return new KValueSimple<T>(initialValue).bindChange(args[0], args[1]);
     case 3:
+      return new KValue<T>(initialValue, args[2], trivial).bindChange(args[0], args[1]);
     case 4:
-
+      return new KValue<T>(initialValue, args[2], args[3]).bindChange(args[0], args[1]);
     default:
       throw new TypeError(`[__NAME__:kvalue] invalid arguments`);
   }
--- src/types/reactive.d.ts
+++ src/types/reactive.d.ts
@@ -1,28 +1,4 @@
 type KRefBound = [HTMLKEnhancedElement, string];
 
-type Transform<A = any, B = any> = (value: A) => B;
-
-type KValueArgs0<T, E extends HTMLKEnhancedElement, ElementValueType = string> = [];
-type KValueArgs1<T, E extends HTMLKEnhancedElement, ElementValueType = string> = [E];
-type KValueArgs2<T, E extends HTMLKEnhancedElement, ElementValueType = string> = [
-  E,
-  ChangeTriggerField | otherstring,
-];
-type KValueArgs3<T, E extends HTMLKEnhancedElement, ElementValueType = string> = [
-  E,
-  ChangeTriggerField | otherstring,
-  Transform<T>,
-];
-// * Full version
-type KValueArgs4<T, E extends HTMLKEnhancedElement, ElementValueType = string> = [
-  E,
-  ChangeTriggerField | otherstring,
-  Transform<T>,
-  Transform<ElementValueType, T>,
-];
-type KValueArgs<T, E extends HTMLKEnhancedElement, ElementValueType = string> =
-  | KValueArgs0<T, E, ElementValueType>
-  | KValueArgs1<T, E, ElementValueType>
-  | KValueArgs2<T, E, ElementValueType>
-  | KValueArgs3<T, E, ElementValueType>
-  | KValueArgs4<T, E, ElementValueType>;
+type TransformTo<A> = (value: unknown) => A;
+type TransformFrom<A> = (value: A) => unknown;

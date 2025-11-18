2592f65c78ca813d3e99d96d89ae8738208cb6dc
aldia_mechdream <futami16237@gmail.com>
2025-11-18 15:06:16 +0800

    feat: 查阅vitest文档后，正确配置了monorepo的测试；现在它们都能正确加载各自的配置了

--- .scripts/vitest.base.config.ts
+++ .scripts/vitest.base.config.ts
@@ -4,18 +4,20 @@ import { ViteUserConfig } from 'vitest/config';
 export const test: ViteUserConfig['test'] = {
   reporters: ['dot'],
   environment: 'jsdom',
-  include: [
-    'tests/**/*.{test,spec,e2e-spec}.?(c|m)[jt]s?(x)',
-    'packages/**/tests/**/*.{test,spec,e2e-spec}.?(c|m)[jt]s?(x)',
-  ],
+  include: ['tests/**/*.{test,spec,e2e-spec}.?(c|m)[jt]s?(x)'],
 };
 
-export const resolve: (dirname: string) => ViteUserConfig['resolve'] = (dirname) => ({
-  alias: {
-    '@': path.resolve(dirname, 'packages/shared/index.ts'),
-    '@ktjs/core': path.resolve(dirname, 'packages/core/src/index.ts'),
-    '@ktjs/router': path.resolve(dirname, 'packages/router/src/main.ts'),
-    '@ktjs/shortcuts': path.resolve(dirname, 'packages/shortcuts/src/index.ts'),
-    '@tests': path.resolve(dirname, 'tests'),
-  },
-});
+export const resolve: (dirname: string) => ViteUserConfig['resolve'] = (dirname) => {
+  const cwd = process.cwd();
+  const currentDirname = path.basename(dirname);
+  console.log('@ means', path.resolve(`packages/${currentDirname}/src/`));
+  return {
+    alias: {
+      '@': path.join(cwd, `packages/${currentDirname}/src/`),
+      '@tests': path.join(cwd, `packages/${currentDirname}/tests`),
+      '@ktjs/core': path.join(cwd, 'packages/core/src/index.ts'),
+      '@ktjs/router': path.join(cwd, 'packages/router/src/main.ts'),
+      '@ktjs/shortcuts': path.join(cwd, 'packages/shortcuts/src/index.ts'),
+    },
+  };
+};
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import { test, resolve } from './.scripts/vitest.base.config';
 
-export default defineConfig({ test, resolve: resolve(import.meta.dirname) });
+export default defineConfig({
+  test: {
+    projects: ['packages/*'],
+  },
+  resolve: resolve(import.meta.dirname),
+});

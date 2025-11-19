2a145c1dc5c731e3722536d092589702dd90c653
aldia_mechdream <futami16237@gmail.com>
2025-11-19 17:33:59 +0800

    chore: 增加todo：完善withdefaults； fix: 修复了core中tsconfig删得太多导致自己不认识自己的@/xx的问题

--- packages/core/src/h/with-defaults.ts
+++ packages/core/src/h/with-defaults.ts
@@ -1,5 +1,6 @@
 import { KTH, KTAttribute } from '@/types/h.js';
 
+// todo 这里要完善，并且要联动shortcuts
 export const withDefaults = <F extends KTH>(func: F, defaultAttr: KTAttribute): F => {
   const newFunc: KTH = (tag, attr, content) => {
     if (typeof attr === 'string') {

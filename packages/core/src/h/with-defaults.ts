7858373fa925eea2c30cab0edc2403df8976a054
aldia_mechdream <futami16237@gmail.com>
2025-11-20 16:48:04 +0800

    feat: 增加ktjs图标； 增加with-defaults函数

--- packages/core/src/h/with-defaults.ts
+++ packages/core/src/h/with-defaults.ts
@@ -1,10 +1,11 @@
 import { KTH, KTAttribute } from '@/types/h.js';
 
-// todo 这里要完善，并且要联动shortcuts
 export const withDefaults = <F extends KTH>(func: F, defaultAttr: KTAttribute): F => {
   const newFunc: KTH = (tag, attr, content) => {
     if (typeof attr === 'string') {
-      attr = { class: attr };
+      attr = { ...defaultAttr, class: attr };
+    } else if (attr && typeof attr === 'object') {
+      attr = { ...defaultAttr, ...attr };
     }
     return func(tag, attr, content);
   };

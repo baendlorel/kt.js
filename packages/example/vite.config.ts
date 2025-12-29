e3aa34f1340e17d90e0de56506864f6a534f8c93
aldia_mechdream <futami16237@gmail.com>
2025-12-29 09:36:14 +0800

    feat: vite配置和tsconfig配置一致后，终于可以运行exapmle了

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -3,12 +3,12 @@ import { resolve } from 'path';
 
 export default defineConfig({
   esbuild: {
-    jsx: 'transform',
-    jsxFactory: 'jsx',
-    jsxFragment: 'Fragment',
+    jsx: 'automatic',
+    jsxImportSource: '@ktjs/core',
   },
   resolve: {
     alias: {
+      '@ktjs/core/jsx-dev-runtime': resolve(import.meta.dirname, '../core/src/jsx/jsx-runtime.ts'),
       '@ktjs/core/jsx-runtime': resolve(import.meta.dirname, '../core/src/jsx/jsx-runtime.ts'),
       '@ktjs/core/jsx': resolve(import.meta.dirname, '../core/src/jsx/index.ts'),
       '@ktjs/core': resolve(import.meta.dirname, '../core/src/index.ts'),

61a35637683aacd2210d7fca5ec50c3962b1b364
aldia_mechdream <futami16237@gmail.com>
2026-01-28 16:21:37 +0800

    feat: 调整代码到src下

---
+++ packages/mui/src/Icons/index.ts
@@ -0,0 +1,26 @@
+// Export all icon components
+export { default as DownloadIcon } from './Download.js';
+export { default as CompressIcon } from './Compress.js';
+export { default as SubtitlesIcon } from './Subtitles.js';
+export { default as SettingsIcon } from './Settings.js';
+export { default as NewReleasesIcon } from './NewReleases.js';
+export { default as FolderOpenIcon } from './FolderOpen.js';
+export { default as PlayArrowIcon } from './PlayArrow.js';
+export { default as ContentPasteIcon } from './ContentPaste.js';
+export { default as DeleteIcon } from './Delete.js';
+export { default as StopIcon } from './Stop.js';
+export { default as FileOpenIcon } from './FileOpen.js';
+export { default as ColorLensIcon } from './ColorLens.js';
+export { default as WallpaperIcon } from './Wallpaper.js';
+export { default as ExpandMoreIcon } from './ExpandMore.js';
+export { default as SaveIcon } from './Save.js';
+export { default as UploadFileIcon } from './UploadFile.js';
+export { default as VideoFileIcon } from './VideoFile.js';
+export { default as QueuePlayNextIcon } from './QueuePlayNext.js';
+export { default as MenuIcon } from './Menu.js';
+export { default as HomeIcon } from './Home.js';
+export { default as ContentCopyIcon } from './ContentCopy.js';
+export { default as SelectAllIcon } from './SelectAll.js';
+
+// Re-export FileOpen as FileOpenIcon alias for compatibility
+export { default as FileOpen } from './FileOpen.js';
---
+++ packages/mui/src/index.ts
@@ -0,0 +1,15 @@
+// Global styles
+import './styles.css';
+
+// UI Components
+export { Alert } from './Alert/Index.js';
+export { Button } from './Button/Index.js';
+export { Dialog } from './Dialog/Index.js';
+export { FormLabel } from './FormLabel/Index.js';
+export { LinearProgress } from './LinearProgress/Index.js';
+export { TextField } from './Input/Index.js';
+export { Radio, RadioGroup } from './Radio/Index.js';
+export { Select } from './Select/Index.js';
+
+// Icons
+export * from './Icons/index.js';

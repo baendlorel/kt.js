6afb386b995f7565bca727ded855dc7a7d025823
aldia_mechdream <futami16237@gmail.com>
2026-01-31 22:29:11 +0800

    chore: 删除alias的检测

--- .scripts/rollup-factory.mjs
+++ .scripts/rollup-factory.mjs
@@ -53,7 +53,6 @@ export function createPackageConfig({
   const aliasOpts = {
     entries: [{ find: /^@\//, replacement: underRoot('packages/core', 'src') + '/' }],
   };
-  console.log('aliasOpts', aliasOpts);
 
   /**
    * @type {import('@rollup/plugin-terser').Options}

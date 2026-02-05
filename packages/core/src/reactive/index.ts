aa124a958912a7dba66e0889ee32c0a220a952a0
aldia_mechdream <futami16237@gmail.com>
2026-02-05 13:36:38 +0800

    feat: 新增toReactive工具，只要管它是不是reactive的就行

--- packages/core/src/reactive/index.ts
+++ packages/core/src/reactive/index.ts
@@ -1,6 +1,14 @@
 import { KTComputed } from './computed.js';
-import { KTRef } from './ref.js';
+import { isKT } from './core.js';
+import { KTRef, ref } from './ref.js';
+
 export type KTReactive<T> = KTRef<T> | KTComputed<T>;
+export const toReactive = <T>(value: T | KTReactive<T>): KTReactive<T> => {
+  if (isKT(value)) {
+    return value;
+  }
+  return ref(value) as KTReactive<T>;
+};
 
 export * from './ref.js';
 export * from './computed.js';

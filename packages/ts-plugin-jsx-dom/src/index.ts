934243389e90076008f42fce256a4303418086ad
aldia_mechdream <futami16237@gmail.com>
2026-02-07 09:09:50 +0800

    feat: 把函数放进init里面

--- packages/ts-plugin-jsx-dom/src/index.ts
+++ packages/ts-plugin-jsx-dom/src/index.ts
@@ -218,188 +218,195 @@ const init: ts.server.PluginModuleFactory = (mod) => {
       return proxy;
     },
   };
+
+  function resolvePreferredTypeText(
+    tsModule: typeof ts,
+    checker: Checker,
+    sourceFile: ts.SourceFile,
+    position: number,
+  ): TypeText {
+    const jsxNode = findNearestJsxNode(tsModule, sourceFile, position);
+    if (jsxNode) {
+      return getJsxNodeTypeText(tsModule, checker, sourceFile, jsxNode);
+    }
+
+    const token = (tsModule as any).getTokenAtPosition(sourceFile, position);
+    if (!token) {
+      return null;
+    }
+
+    const symbol = checker.getSymbolAtLocation(token);
+    if (!symbol) {
+      return null;
+    }
+
+    for (const decl of symbol.getDeclarations() || []) {
+      const initializer = getInitializerFromDeclaration(tsModule, decl);
+      if (initializer && isJsxNode(tsModule, initializer)) {
+        return getJsxNodeTypeText(tsModule, checker, sourceFile, initializer);
+      }
+    }
+
+    return null;
+  }
+
+  function findNearestJsxNode(tsModule: typeof ts, sourceFile: ts.SourceFile, position: number): JsxNode | null {
+    const token = (tsModule as any).getTokenAtPosition(sourceFile, position);
+    if (!token) {
+      return null;
+    }
+
+    let current: ts.Node | undefined = token;
+    while (current) {
+      if (isJsxNode(tsModule, current)) return current;
+      current = current.parent;
+    }
+
+    return null;
+  }
+
+  function isJsxNode(tsModule: typeof ts, node: ts.Node): node is JsxNode {
+    return tsModule.isJsxElement(node) || tsModule.isJsxSelfClosingElement(node) || tsModule.isJsxFragment(node);
+  }
+
+  function getInitializerFromDeclaration(tsModule: typeof ts, decl: ts.Declaration): ts.Expression | null {
+    if (tsModule.isVariableDeclaration(decl) || tsModule.isPropertyDeclaration(decl)) {
+      return decl.initializer ?? null;
+    }
+
+    if (tsModule.isParameter(decl)) {
+      return decl.initializer ?? null;
+    }
+
+    return null;
+  }
+
+  function getJsxNodeTypeText(
+    tsModule: typeof ts,
+    checker: Checker,
+    sourceFile: ts.SourceFile,
+    node: JsxNode,
+  ): TypeText {
+    if (tsModule.isJsxFragment(node)) return null;
+
+    const tagName = getTagNameFromJsx(tsModule, node);
+    if (!tagName) return null;
+
+    if (tsModule.isIdentifier(tagName)) {
+      if (isIntrinsicTag(tagName.text)) {
+        return getIntrinsicElementTypeText(tsModule, checker, sourceFile, tagName.text);
+      }
+      return getComponentReturnTypeText(tsModule, checker, tagName);
+    }
+
+    if (tsModule.isPropertyAccessExpression(tagName)) {
+      return getComponentReturnTypeText(tsModule, checker, tagName);
+    }
+
+    if (tsModule.isJsxNamespacedName(tagName)) {
+      const localName = tagName.name.text;
+      return getIntrinsicElementTypeText(tsModule, checker, sourceFile, localName);
+    }
+
+    return null;
+  }
+
+  function getTagNameFromJsx(
+    tsModule: typeof ts,
+    node: ts.JsxElement | ts.JsxSelfClosingElement,
+  ): ts.JsxTagNameExpression {
+    if (tsModule.isJsxElement(node)) {
+      return node.openingElement.tagName;
+    }
+    return node.tagName;
+  }
+
+  function isIntrinsicTag(tagName: string): boolean {
+    return tagName[0] === tagName[0].toLowerCase();
+  }
+
+  function getIntrinsicElementTypeText(
+    tsModule: typeof ts,
+    checker: Checker,
+    sourceFile: ts.SourceFile,
+    tagName: string,
+  ): TypeText {
+    const htmlType = getElementTypeFromMap(tsModule, checker, sourceFile, 'HTMLElementTagNameMap', tagName);
+    if (htmlType) {
+      return htmlType;
+    }
+
+    const svgType = getElementTypeFromMap(tsModule, checker, sourceFile, 'SVGElementTagNameMap', tagName);
+    if (svgType) {
+      return svgType;
+    }
+
+    return null;
+  }
+
+  function getElementTypeFromMap(
+    tsModule: typeof ts,
+    checker: Checker,
+    sourceFile: ts.SourceFile,
+    mapName: string,
+    tagName: string,
+  ): TypeText {
+    const mapSymbol = checker
+      .getSymbolsInScope(sourceFile, tsModule.SymbolFlags.Interface)
+      .find((symbol) => symbol.getName() === mapName);
+
+    if (!mapSymbol) {
+      return null;
+    }
+
+    const mapType = checker.getTypeOfSymbolAtLocation(mapSymbol, sourceFile);
+    const propSymbol = mapType.getProperty(tagName);
+    if (!propSymbol) {
+      return null;
+    }
+
+    const propType = checker.getTypeOfSymbolAtLocation(propSymbol, sourceFile);
+    return checker.typeToString(propType, sourceFile, tsModule.TypeFormatFlags.NoTruncation);
+  }
+
+  function getComponentReturnTypeText(
+    tsModule: typeof ts,
+    checker: Checker,
+    tagName: ts.JsxTagNameExpression,
+  ): TypeText {
+    const symbol = checker.getSymbolAtLocation(tagName);
+    if (!symbol) {
+      return null;
+    }
+
+    const targetSymbol = symbol.flags & tsModule.SymbolFlags.Alias ? checker.getAliasedSymbol(symbol) : symbol;
+
+    const type = checker.getTypeOfSymbolAtLocation(targetSymbol, tagName);
+    const signatures = type.getCallSignatures();
+    const signature = signatures[0] || type.getConstructSignatures()[0];
+    if (!signature) {
+      return null;
+    }
+
+    const returnType = checker.getReturnTypeOfSignature(signature);
+    return checker.typeToString(returnType, tagName, tsModule.TypeFormatFlags.NoTruncation);
+  }
+
+  function findNodeAtPosition(tsModule: typeof ts, sourceFile: ts.SourceFile, position: number): ts.Node | undefined {
+    // 使用getTokenAtPosition找到位置附近的token
+    const token = (tsModule as any).getTokenAtPosition(sourceFile, position);
+    if (!token) {
+      return undefined;
+    }
+
+    // 找到包含该位置的节点
+    let node: ts.Node | undefined = token;
+    while (node && (position < node.pos || position >= node.end)) {
+      node = node.parent;
+    }
+
+    return node || undefined;
+  }
 };
-
-function resolvePreferredTypeText(
-  tsModule: typeof ts,
-  checker: Checker,
-  sourceFile: ts.SourceFile,
-  position: number,
-): TypeText {
-  const jsxNode = findNearestJsxNode(tsModule, sourceFile, position);
-  if (jsxNode) {
-    return getJsxNodeTypeText(tsModule, checker, sourceFile, jsxNode);
-  }
-
-  const token = (tsModule as any).getTokenAtPosition(sourceFile, position);
-  if (!token) {
-    return null;
-  }
-
-  const symbol = checker.getSymbolAtLocation(token);
-  if (!symbol) {
-    return null;
-  }
-
-  for (const decl of symbol.getDeclarations() || []) {
-    const initializer = getInitializerFromDeclaration(tsModule, decl);
-    if (initializer && isJsxNode(tsModule, initializer)) {
-      return getJsxNodeTypeText(tsModule, checker, sourceFile, initializer);
-    }
-  }
-
-  {
-    return null;
-  }
-}
-
-function findNearestJsxNode(tsModule: typeof ts, sourceFile: ts.SourceFile, position: number): JsxNode | null {
-  const token = (tsModule as any).getTokenAtPosition(sourceFile, position);
-  if (!token) {
-    return null;
-  }
-
-  let current: ts.Node | undefined = token;
-  while (current) {
-    if (isJsxNode(tsModule, current)) return current;
-    current = current.parent;
-  }
-
-  return null;
-}
-
-function isJsxNode(tsModule: typeof ts, node: ts.Node): node is JsxNode {
-  return tsModule.isJsxElement(node) || tsModule.isJsxSelfClosingElement(node) || tsModule.isJsxFragment(node);
-}
-
-function getInitializerFromDeclaration(tsModule: typeof ts, decl: ts.Declaration): ts.Expression | null {
-  if (tsModule.isVariableDeclaration(decl) || tsModule.isPropertyDeclaration(decl)) {
-    return decl.initializer ?? null;
-  }
-
-  if (tsModule.isParameter(decl)) {
-    return decl.initializer ?? null;
-  }
-
-  return null;
-}
-
-function getJsxNodeTypeText(tsModule: typeof ts, checker: Checker, sourceFile: ts.SourceFile, node: JsxNode): TypeText {
-  if (tsModule.isJsxFragment(node)) return null;
-
-  const tagName = getTagNameFromJsx(tsModule, node);
-  if (!tagName) return null;
-
-  if (tsModule.isIdentifier(tagName)) {
-    if (isIntrinsicTag(tagName.text)) {
-      return getIntrinsicElementTypeText(tsModule, checker, sourceFile, tagName.text);
-    }
-    return getComponentReturnTypeText(tsModule, checker, tagName);
-  }
-
-  if (tsModule.isPropertyAccessExpression(tagName)) {
-    return getComponentReturnTypeText(tsModule, checker, tagName);
-  }
-
-  if (tsModule.isJsxNamespacedName(tagName)) {
-    const localName = tagName.name.text;
-    return getIntrinsicElementTypeText(tsModule, checker, sourceFile, localName);
-  }
-
-  return null;
-}
-
-function getTagNameFromJsx(
-  tsModule: typeof ts,
-  node: ts.JsxElement | ts.JsxSelfClosingElement,
-): ts.JsxTagNameExpression {
-  if (tsModule.isJsxElement(node)) {
-    return node.openingElement.tagName;
-  }
-  return node.tagName;
-}
-
-function isIntrinsicTag(tagName: string): boolean {
-  return tagName[0] === tagName[0].toLowerCase();
-}
-
-function getIntrinsicElementTypeText(
-  tsModule: typeof ts,
-  checker: Checker,
-  sourceFile: ts.SourceFile,
-  tagName: string,
-): TypeText {
-  const htmlType = getElementTypeFromMap(tsModule, checker, sourceFile, 'HTMLElementTagNameMap', tagName);
-  if (htmlType) {
-    return htmlType;
-  }
-
-  const svgType = getElementTypeFromMap(tsModule, checker, sourceFile, 'SVGElementTagNameMap', tagName);
-  if (svgType) {
-    return svgType;
-  }
-
-  return null;
-}
-
-function getElementTypeFromMap(
-  tsModule: typeof ts,
-  checker: Checker,
-  sourceFile: ts.SourceFile,
-  mapName: string,
-  tagName: string,
-): TypeText {
-  const mapSymbol = checker
-    .getSymbolsInScope(sourceFile, tsModule.SymbolFlags.Interface)
-    .find((symbol) => symbol.getName() === mapName);
-
-  if (!mapSymbol) {
-    return null;
-  }
-
-  const mapType = checker.getTypeOfSymbolAtLocation(mapSymbol, sourceFile);
-  const propSymbol = mapType.getProperty(tagName);
-  if (!propSymbol) {
-    return null;
-  }
-
-  const propType = checker.getTypeOfSymbolAtLocation(propSymbol, sourceFile);
-  return checker.typeToString(propType, sourceFile, tsModule.TypeFormatFlags.NoTruncation);
-}
-
-function getComponentReturnTypeText(tsModule: typeof ts, checker: Checker, tagName: ts.JsxTagNameExpression): TypeText {
-  const symbol = checker.getSymbolAtLocation(tagName);
-  if (!symbol) {
-    return null;
-  }
-
-  const targetSymbol = symbol.flags & tsModule.SymbolFlags.Alias ? checker.getAliasedSymbol(symbol) : symbol;
-
-  const type = checker.getTypeOfSymbolAtLocation(targetSymbol, tagName);
-  const signatures = type.getCallSignatures();
-  const signature = signatures[0] || type.getConstructSignatures()[0];
-  if (!signature) {
-    return null;
-  }
-
-  const returnType = checker.getReturnTypeOfSignature(signature);
-  return checker.typeToString(returnType, tagName, tsModule.TypeFormatFlags.NoTruncation);
-}
-
-function findNodeAtPosition(tsModule: typeof ts, sourceFile: ts.SourceFile, position: number): ts.Node | undefined {
-  // 使用getTokenAtPosition找到位置附近的token
-  const token = (tsModule as any).getTokenAtPosition(sourceFile, position);
-  if (!token) {
-    return undefined;
-  }
-
-  // 找到包含该位置的节点
-  let node: ts.Node | undefined = token;
-  while (node && (position < node.pos || position >= node.end)) {
-    node = node.parent;
-  }
-
-  return node || undefined;
-}
 
 export = init;

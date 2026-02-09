cc4438da8460c151d69d5a0c8dfc6d0c16cf3312
aldia_mechdream <futami16237@gmail.com>
2026-02-09 08:30:11 +0800

    release: shared 0.23.5 发布$开头的parsestyle和applymodel； 以及增加$forEach和async版

--- packages/shared/src/utils/misc.ts
+++ packages/shared/src/utils/misc.ts
@@ -13,3 +13,29 @@ export const $isSame = (a: unknown, b: unknown): boolean => {
   }
   return $is(a, b);
 };
+
+/**
+ * Safe and quick forEach implementation that works with array-like objects and handles sparse arrays.
+ */
+export const $forEach = (
+  array: unknown[],
+  callback: (item: unknown, index: number, array: unknown[]) => void,
+): void => {
+  const len = array.length;
+  for (let i = 0; i < len; i++) {
+    callback(array[i], i, array);
+  }
+};
+
+/**
+ * Async version of $forEach that allows for asynchronous callbacks. It processes items sequentially, awaiting each callback before moving to the next.
+ */
+export const $forEachAsync = async (
+  array: unknown[],
+  callback: (item: unknown, index: number, array: unknown[]) => void,
+): Promise<void> => {
+  const len = array.length;
+  for (let i = 0; i < len; i++) {
+    await callback(array[i], i, array);
+  }
+};

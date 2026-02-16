2a69c101683cd6a0389b936fbd06e81402868960
aldia_mechdream <futami16237@gmail.com>
2026-02-16 10:06:27 +0800

    feat: 不知道为什么不行

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,8 +3,5 @@ import { getPackageInfo } from './common/package-info.js';
 
 export async function build(who: string | undefined) {
   const info = getPackageInfo(who);
-  execSync(`pnpm exec tsx ./node_modules/rollup/dist/bin/rollup -c rollup.config.ts`, {
-    stdio: 'inherit',
-    env: info.env,
-  });
+  execSync(`rollup -c rollup.config.ts --configPlugin typescript`, { stdio: 'inherit', env: info.env });
 }

c982a1f628a5be67b1acaaea65780a7cf9aa7062
aldia_mechdream <futami16237@gmail.com>
2026-02-18 16:16:35 +0800

    release 0.29.0 打包脚本简化

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,14 +4,22 @@ import { build } from './build.js';
 import { publish } from './publish.js';
 import { test } from './test.js';
 
-const task = process.argv[2];
-const [who] = process.argv.slice(3);
-if (task === '--publish') {
-  publish(who);
-} else if (task === '--build') {
-  build(who);
-} else if (task === '--test') {
-  test(who);
-} else {
-  console.error(`Unknown task: ${task}`);
+async function main() {
+  const task = process.argv[2];
+  const [who] = process.argv.slice(3);
+  if (task === '--publish') {
+    if (who === undefined) {
+      await publish('core');
+      await publish('kt.js');
+    } else {
+      publish(who);
+    }
+  } else if (task === '--build') {
+    build(who);
+  } else if (task === '--test') {
+    test(who);
+  } else {
+    console.error(`Unknown task: ${task}`);
+  }
 }
+main();

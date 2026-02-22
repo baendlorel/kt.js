dd32595f16771848d783abbad9a022eaef963573
aldia_mechdream <futami16237@gmail.com>
2026-02-22 22:00:20 +0800

    feat: 使用ktconditional来处理元素创建

--- packages/core/src/jsx/if.ts
+++ packages/core/src/jsx/if.ts
@@ -82,3 +82,33 @@ export function kifelseApply(el: HTMLElement) {
     }
   }
 }
+
+export function KTConditional(
+  condition: boolean | KTReactive<boolean>,
+  tagIf: JSXTag,
+  propsIf: KTAttribute,
+  tagElse?: JSXTag,
+  propsElse?: KTAttribute,
+) {
+  if (!isKT(condition)) {
+    return condition ? jsxh(tagIf, propsIf) : tagElse ? jsxh(tagElse, propsElse!) : placeholder('kt-conditional');
+  }
+
+  if (tagElse) {
+    let current = condition.value ? jsxh(tagIf, propsIf) : jsxh(tagElse!, propsElse!);
+    condition.addOnChange((newValue) => {
+      const old = current;
+      current = newValue ? jsxh(tagIf, propsIf) : jsxh(tagElse!, propsElse!);
+      $replaceNode(old, current);
+    });
+    return current;
+  } else {
+    const dummy = placeholder('kt-conditional') as HTMLElement;
+    condition.addOnChange((newValue) => {
+      const old = dummy;
+      const newEl = newValue ? jsxh(tagIf, propsIf) : dummy;
+      $replaceNode(old, newEl);
+    });
+    return condition.value ? jsxh(tagIf, propsIf) : dummy;
+  }
+}

4c184fb6bc1d6d732ace8bb5357560352fe07b74
aldia_mechdream <futami16237@gmail.com>
2026-02-23 07:01:56 +0800

    feat: 精简了checkbox

--- packages/mui/src/components/Checkbox/Checkbox.tsx
+++ packages/mui/src/components/Checkbox/Checkbox.tsx
@@ -7,34 +7,31 @@ import { createUnchecked, createChecked, createIndeterminate } from './Icons.js'
 export function Checkbox(props: KTMuiCheckboxProps): KTMuiCheckbox {
   // Handle change
   const handleChange = () => {
-    if (disabledRef.value) {
+    if (disabled.value) {
       return;
     }
-    modelRef.value = inputEl.checked;
-    interminateRef.value = false;
-    onChange(modelRef.value, valueRef.value);
+    model.value = inputEl.checked;
+    interminate.value = false;
+    onChange(model.value, value.value);
   };
 
   const onChange = props['on:change'] ?? $emptyFn;
 
-  const labelRef = toReactive(props.label ?? '');
-  const valueRef = toReactive(props.value ?? '');
-  const interminateRef = toReactive(props.indeterminate ?? false);
-  const colorRef = toReactive(props.color ?? 'primary');
-  const sizeRef = toReactive(props.size ?? 'medium');
-  const disabledRef = toReactive(props.disabled ?? false, (v) => {
-    inputEl.disabled = v;
-    container.classList.toggle('mui-checkbox-disabled', v);
-  });
-  const modelRef = $modelOrRef(props, props.checked ?? false);
+  const label = toReactive(props.label ?? '');
+  const value = toReactive(props.value ?? '');
+  const interminate = toReactive(props.indeterminate ?? false);
+  const color = toReactive(props.color ?? 'primary');
+  const size = toReactive(props.size ?? 'medium');
+  const disabled = toReactive(props.disabled ?? false);
+  const model = $modelOrRef(props, props.checked ?? false);
 
   const inputEl = (
     <input
       type="checkbox"
       class="mui-checkbox-input"
-      checked={modelRef}
-      value={valueRef}
-      disabled={disabledRef}
+      checked={model}
+      value={value}
+      disabled={disabled}
       on:change={handleChange}
     />
   ) as HTMLInputElement;
@@ -43,35 +40,35 @@ export function Checkbox(props: KTMuiCheckboxProps): KTMuiCheckbox {
   const checkedIcon = createChecked();
   const indeterminateIcon = createIndeterminate();
 
-  const classRef = computed(() => {
-    return `mui-checkbox-wrapper mui-checkbox-size-${sizeRef.value} ${disabledRef.value ? 'mui-checkbox-disabled' : ''} mui-checkbox-color-${colorRef.value}`;
-  }, [colorRef, disabledRef, sizeRef]);
+  const className = computed(() => {
+    return `mui-checkbox-wrapper mui-checkbox-size-${size.value} ${disabled.value ? 'mui-checkbox-disabled' : ''} mui-checkbox-color-${color.value}`;
+  }, [color, disabled, size]);
 
   const container = (
-    <label class={classRef}>
+    <label class={className}>
       {inputEl}
       <span class="mui-checkbox-icon">
         {uncheckedIcon}
         {checkedIcon}
         {indeterminateIcon}
       </span>
-      <span k-if={labelRef} class="mui-checkbox-label">
-        {labelRef}
+      <span k-if={label} class="mui-checkbox-label">
+        {label}
       </span>
     </label>
   ) as KTMuiCheckbox;
 
   effect(() => {
-    if (interminateRef.value) {
+    if (interminate.value) {
       uncheckedIcon.style.display = 'none';
       checkedIcon.style.display = 'none';
       indeterminateIcon.style.display = '';
     } else {
-      uncheckedIcon.style.display = modelRef.value ? 'none' : '';
-      checkedIcon.style.display = modelRef.value ? '' : 'none';
+      uncheckedIcon.style.display = model.value ? 'none' : '';
+      checkedIcon.style.display = model.value ? '' : 'none';
       indeterminateIcon.style.display = 'none';
     }
-  }, [modelRef, interminateRef]);
+  }, [model, interminate]);
 
   return container;
 }

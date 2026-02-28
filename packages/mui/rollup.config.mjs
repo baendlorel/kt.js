33746e6791ea4f3667bdcf6a16ce85af38bccf4f
aldia_mechdream <futami16237@gmail.com>
2026-02-28 16:45:05 +0800

    chore: 尝试改回去

--- packages/mui/rollup.config.mjs
+++ packages/mui/rollup.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import path from 'node:path';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -7,15 +8,12 @@ import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import { rimraf } from 'rimraf';
 
-import { aggregateCss, ignoreCssImports } from './aggregate-css.mjs';
-
 const pkgDir = path.resolve(import.meta.dirname);
 const srcEntry = path.join(pkgDir, 'src', 'index.ts');
 const distDir = path.join(pkgDir, 'dist');
 
 export default async () => {
   await rimraf(distDir);
-  await aggregateCss();
   return [
     {
       input: srcEntry,
@@ -34,7 +32,6 @@ export default async () => {
           declarationMap: false,
           sourceMap: true,
         }),
-        ignoreCssImports(),
         terser(),
       ],
       external: [/^@ktjs\//],
@@ -45,7 +42,7 @@ export default async () => {
         file: path.join(distDir, 'index.d.ts'),
         format: 'es',
       },
-      plugins: [ignoreCssImports(), dts()],
+      plugins: [dts()],
       external: [/^@ktjs\//],
     },
   ];

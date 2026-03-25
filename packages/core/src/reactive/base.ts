281e318ae9eee4ca161324046c91b346d86e2d92
aldia_mechdream <futami16237@gmail.com>
2026-03-25 15:54:46 +0800

    feat: 增加了核心的KTDerivable，它决定了reactive和subreactive的关系，但泛型有点难写

--- packages/core/src/reactive/base.ts
+++ packages/core/src/reactive/base.ts
@@ -8,8 +8,10 @@ type ChangeHandler<T> = (newValue: T, oldValue: T) => void;
 
 interface KTReactiveBase<T> {
   readonly isKT: true;
+
   readonly type: KTReactiveType;
-  get value(): T; // & Definitely readable
+
+  readonly value: T; // & Reactive objects must at least be readable
 }
 
 interface KTWritable<T> {
@@ -50,5 +52,61 @@ interface KTListenable<T> {
   notify(): this;
 }
 
+interface KTDerivable<T> {
+  /**
+   * Generate a computed value based on this ref, using keys to access nested properties.
+   * - `ref.get('a', 'b')` is equivalent to `ref.map((v) => v.a.b)`, but simpler to write.
+   * @throws when `a.b.c` throws error(e.g. `a.b` is undefined, then it throws when calling `undefined.c`).
+   */
+  get<
+    K0 extends keyof T,
+    K1 extends keyof T[K0],
+    K2 extends keyof T[K0][K1],
+    K3 extends keyof T[K0][K1][K2],
+    K4 extends keyof T[K0][K1][K2][K3],
+  >(
+    key0: K0,
+    key1: K1,
+    key2: K2,
+    key3: K3,
+    key4: K4,
+  ): Derived<T[K0][K1][K2][K3][K4]>;
+  /**
+   * Generate a computed value based on this ref, using keys to access nested properties.
+   * - `ref.get('a', 'b')` is equivalent to `ref.map((v) => v.a.b)`, but simpler to write.
+   * @throws when `a.b.c` throws error(e.g. `a.b` is undefined, then it throws when calling `undefined.c`).
+   */
+  get<K0 extends keyof T, K1 extends keyof T[K0], K2 extends keyof T[K0][K1], K3 extends keyof T[K0][K1][K2]>(
+    key0: K0,
+    key1: K1,
+    key2: K2,
+    key3: K3,
+  ): KTReactiveBase<T[K0][K1][K2][K3]>;
+  /**
+   * Generate a computed value based on this ref, using keys to access nested properties.
+   * - `ref.get('a', 'b')` is equivalent to `ref.map((v) => v.a.b)`, but simpler to write.
+   * @throws when `a.b.c` throws error(e.g. `a.b` is undefined, then it throws when calling `undefined.c`).
+   */
+  get<K0 extends keyof T, K1 extends keyof T[K0], K2 extends keyof T[K0][K1]>(
+    key0: K0,
+    key1: K1,
+    key2: K2,
+  ): KTReactiveBase<T[K0][K1][K2]>;
+  /**
+   * Generate a computed value based on this ref, using keys to access nested properties.
+   * - `ref.get('a', 'b')` is equivalent to `ref.map((v) => v.a.b)`, but simpler to write.
+   * @throws when `a.b.c` throws error(e.g. `a.b` is undefined, then it throws when calling `undefined.c`).
+   */
+  get<K0 extends keyof T, K1 extends keyof T[K0]>(key0: K0, key1: K1): KTReactiveBase<T[K0][K1]>;
+  /**
+   * Generate a computed value based on this ref, using keys to access nested properties.
+   * - `ref.get('a', 'b')` is equivalent to `ref.map((v) => v.a.b)`, but simpler to write.
+   * @throws when `a.b.c` throws error(e.g. `a.b` is undefined, then it throws when calling `undefined.c`).
+   */
+  get<K0 extends keyof T>(key0: K0): KTReactiveBase<T[K0]>;
+}
+
 type KTComputed<T> = KTReactiveBase<T> & KTListenable<T> & KTMappable<T>;
 type KTRef<T> = KTReactiveBase<T> & KTListenable<T> & KTMappable<T> & KTWritable<T>;
+type KTSubRef<T> = KTReactiveBase<T> & KTWritable<T>;
+type KTSubComputed<T> = KTReactiveBase<T>;

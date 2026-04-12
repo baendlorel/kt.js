957c887a4367cd01ceb75b13b4c6058f25d26d09
aldia_mechdream <futami16237@gmail.com>
2026-04-12 21:33:14 +0800

    feat: 创造性地编写了indom函数判定，

--- packages/shared/src/utils/dom.ts
+++ packages/shared/src/utils/dom.ts
@@ -35,3 +35,20 @@ export const $parseStyle = (style: unknown): string => {
 };
 
 export type ChangeHandler<T = string> = (value: T, ...args: any[]) => void;
+
+/**
+ * [INFO]  document.contains\isConnected\getRootNode can be used to check if an element is still in the DOM.
+ * - `document.contains` is widely supported but slow.
+ * - `node.isConnected` is much faster but not supported in IE.
+ *
+ * So we combine them as this function
+ */
+export const $inDom: (node: Node) => boolean = (() => {
+  const descriptor = Object.getOwnPropertyDescriptor(Node.prototype, 'isConnected');
+
+  if (descriptor && descriptor.get) {
+    const isConnected = descriptor.get;
+    return (node) => isConnected.call(node);
+  }
+  return (node) => document.contains(node);
+})();

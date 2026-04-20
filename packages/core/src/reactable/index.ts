975e337b8f785177b3a1e1f10332532ad8dacfd8
aldia_mechdream <futami16237@gmail.com>
2026-04-20 16:17:26 +0800

    refactor: 简化toReactive函数实现

--- packages/core/src/reactable/index.ts
+++ packages/core/src/reactable/index.ts
@@ -3,18 +3,13 @@ import { isKT } from './common.js';
 import { KTRef } from './ref.js';
 
 /**
- * Ensure a value is reactive. If it's already `KTReactiveLike`, return it as is; otherwise, wrap it in a `ref`.
+ * @returns it as is when it's already `KTReactive`. Otherwise wraps it with `ref`.
  */
-export const toReactive = <T>(o: T | KTReactive<T>): KTReactive<T> =>
-  isKT(o) ? o : (new KTRef(o as T) as KTReactive<T>);
-
-/**
- * Extracts the value from a KTReactive, or returns the value directly if it's not reactive.
- */
-export const dereactive = <T>(value: T | KTReactive<T>): T => (isKT<T>(value) ? value.value : value);
+export const toReactive = <T>(o: T | KTReactive<T>): KTReactive<T> => (isKT<T>(o) ? o : new KTRef(o as T));
 
 export type { KTRef, KTSubRef } from './ref.js';
 export { ref, assertModel } from './ref.js';
+
 export type { KTComputed, KTComputedLike } from './computed.js';
 export { computed } from './computed.js';
 
@@ -22,5 +17,6 @@ export { KType } from './reactive.js';
 export type { KTReactive } from './reactive.js';
 
 export { isKT, isReactiveLike, isRef, isSubRef, isRefExact, isComputed, isReactive } from './common.js';
+
 export { effect } from './effect.js';
 export type * from './types.js';

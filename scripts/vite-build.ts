39e6c3d35f0b5debef2f35e3acde2c92744a2527
aldia_mechdream <futami16237@gmail.com>
2026-04-27 16:49:14 +0800

    chore: 现在packages下的都可以用vite打包了

--- scripts/vite-build.ts
+++ scripts/vite-build.ts
@@ -10,10 +10,11 @@ export function vitebuild(who: string | undefined) {
 
 const config = dirs.root.join('configs', 'vite-build', 'vite.config.ts');
 
+const specialLibs = ['@ktjs/ts-plugin', '@ktjs/kt-tsc', '@ktjs/example'];
+
 export function buildWithInfo(info: PackageInfo) {
   console.log(`Vite Building package: ${info.name}`);
-  // , '@ktjs/mui'
-  if (['@ktjs/ts-plugin', '@ktjs/kt-tsc', '@ktjs/example'].includes(info.name)) {
+  if (specialLibs.includes(info.name)) {
     execSync(`pnpm --filter ${info.name} run build`, { stdio: 'inherit', env: info.env });
   } else {
     execSync(`vite build --config ${config.safe()} ${info.path.safe()}`, { stdio: 'inherit', env: info.env });

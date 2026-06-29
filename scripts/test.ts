7f9520bae41f6d568054f09e0a1959bdae00e208
aldia_mechdream <futami16237@gmail.com>
2026-06-29 16:02:26 +0800

    feat: 修复了test命令；

--- scripts/test.ts
+++ scripts/test.ts
@@ -4,7 +4,7 @@ import { getPackageInfo } from './package-info.js';
 
 export async function test(who: string | undefined) {
   getPackageInfo(who).forEach((pkg) => {
-    const vitestConfigPath = dirs.configs.join('vitest.config.ts');
+    const vitestConfigPath = dirs.root.join('vitest.config.ts');
 
     execSync(`vitest ${pkg.path.safe()} --config ${vitestConfigPath.safe()} --passWithNoTests`, {
       stdio: 'inherit',
